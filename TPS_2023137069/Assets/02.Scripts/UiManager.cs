d366b137e414322ac2d3b8fc8155e232962e3a4d
Kaeny <shh020615@gmail.com>
2023-05-31 15:19:47 +0900

    핑크 개구리 군단

---
+++ TPS_2023137069/Assets/02.Scripts/UiManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+public void OnButtonClick()
+    {
+        Debug.Log("²Ú²Ú");
+    }
+}
